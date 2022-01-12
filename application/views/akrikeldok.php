d0d6ec24dc857ea829545194d725323dd90a6726
nadivaraa <nadivaramandani.nda@gmail.com>
2022-01-12 19:12:58 +0700

    hapus bookmark tab

--- application/views/akrikeldok.php
+++ application/views/akrikeldok.php
@@ -53,28 +53,6 @@
     <!-- BEGIN: Header-->
     <nav class="header-navbar navbar navbar-expand-lg align-items-center floating-nav navbar-light navbar-shadow container-xxl">
         <div class="navbar-container d-flex content">
-            <div class="bookmark-wrapper d-flex align-items-center">
-                <ul class="nav navbar-nav d-xl-none">
-                    <li class="nav-item"><a class="nav-link menu-toggle" href="#"><i class="ficon" data-feather="menu"></i></a></li>
-                </ul>
-                <ul class="nav navbar-nav bookmark-icons">
-                    <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-email.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Dashboard"><i class="ficon" data-feather="home"></i></a></li>
-                    <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-chat.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nasabah"><i class="ficon" data-feather="user"></i></a></li>
-                    <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-todo.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="VerifBerkas"><i class="ficon" data-feather="check-circle"></i></a></li> 
-                    <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-email.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Kriteria"><i class="ficon" data-feather="file"></i></a></li>
-                    <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-chat.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Penilaian"><i class="ficon" data-feather="check-square"></i></a></li>
-                    <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-todo.html" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Rekomendasi"><i class="ficon" data-feather="award"></i></a></li>
-                </ul>
-                <ul class="nav navbar-nav">
-                    <li class="nav-item d-none d-lg-block"><a class="nav-link bookmark-star"><i class="ficon text-warning" data-feather="star"></i></a>
-                        <div class="bookmark-input search-input">
-                            <div class="bookmark-input-icon"><i data-feather="search"></i></div>
-                            <input class="form-control input" type="text" placeholder="Bookmark" tabindex="0" data-search="search">
-                            <ul class="search-list search-list-bookmark"></ul>
-                        </div>
-                    </li>
-                </ul>
-            </div>
             <ul class="nav navbar-nav align-items-center ms-auto">
                 
                 
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -11,7 +11,7 @@
     <meta name="author" content="PIXINVENT">
     <title>Login user</title>
     <link rel="apple-touch-icon" href="<?= base_url()?>/assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url()?>/assets/images/ico/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url()?>/assets/images/logo/logokpr.png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
